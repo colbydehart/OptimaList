77fd9443dbad79323cb680d5c24d42745fef690f
Colby DeHart <colbydehart@gmail.com>
2015-03-31 14:37:37 -0500

    made models

--- optimaList/models.py
+++ optimaList/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=60, blank=False)
+    url = models.URLField()
+    user = models.ForeignKey(User)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=60, blank=False)
+
+
+class RecipeItem(models.Model):
+    recipe = models.ForeignKey(Recipe)
+    ingredient = models.ForeignKey(Ingredient)
+    measurement = models.CharField(max_length=60)
+    quantity = models.DecimalField()
