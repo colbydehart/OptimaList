4cab3d123fed535e2af143fab6cf1fca31b7a58b
Colby DeHart <colbydehart@gmail.com>
2015-04-02 11:08:15 -0500

    fixing static files

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,15 +2,30 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     $ = require('gulp-load-plugins')();
 
+paths = {
+    js: [
+            'client/vendor/jquery/dist/jquery.js',
+            'client/vendor/bootstrap/dist/js/bootstrap.js',
+            'client/vendor/lodash/dist/lodash.js',
+            'client/vendor/angular/angular.js',
+            'client/vendor/angular-route/angular-route.js',
+            'client/vendor/restangular/dist/restangular.js',
+            'client/vendor/angular-local-storage/dist/angular-local-storage.js',
+            'client/scripts/main.js',
+            'client/scripts/*.js'
+        ],
+    sass: 'client/sass/main.scss'
+};
+
 gulp.task('sass', [], function() {
-    gulp.src('client/sass/main.scss')
+    gulp.src(paths.sass)
     .pipe($.plumber())
     .pipe($.sass())
     .pipe(gulp.dest('optimaList/static/optimaList/content'));
 });
 
 gulp.task('js', [], function(){
-    gulp.src(['client/scripts/main.js','client/scripts/*.js'])
+    gulp.src(paths.js)
     .pipe($.plumber())
     .pipe($.concat('app.js'))
     .pipe(gulp.dest('optimaList/static/optimaList/content'));
