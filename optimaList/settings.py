d73dced14e26b6e3cfb4aa8a6adb3c7755e0fc49
Colby DeHart <colbydehart@gmail.com>
2015-04-17 14:17:57 -0500

    changed db settings

--- optimaList/settings.py
+++ optimaList/settings.py
@@ -68,15 +68,6 @@ ROOT_URLCONF = 'optimaList.urls'
 WSGI_APPLICATION = 'optimaList.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
