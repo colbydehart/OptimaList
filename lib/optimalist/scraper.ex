1a2ea952eff2ad14e53c26e1576e49d756de5784
Colby DeHart <colbydehart@gmail.com>
2018-10-16 15:34:51 -0500

    normalize measurements and amounts

--- lib/optimalist/scraper.ex
+++ lib/optimalist/scraper.ex
@@ -48,19 +48,62 @@ defmodule Optimalist.Scraper do
            HTTPoison.post(url, Poison.encode!(%{"ingredients" => strings}), headers),
          {:ok, %{"results" => results}} <- Poison.decode(body) do
       Enum.map(results, fn res ->
-        %{name: res["name"], measurement: res["unit"], amount: float_or_zero(res["qty"])}
+        %{
+          name: res["name"],
+          measurement: normalize_measurement(res["unit"]),
+          amount: normalize_amount(res["qty"])
+        }
       end)
     else
       _ -> []
     end
   end
 
-  defp float_or_zero(nil), do: 0.0
+  def normalize_amount(nil), do: 0.0
+  def normalize_amount(""), do: 1.0
+  def normalize_amount("½"), do: 0.5
+  def normalize_amount("¾"), do: 0.75
+  def normalize_amount("¼"), do: 0.25
+  def normalize_amount("⅓"), do: 0.33
+  def normalize_amount("⅔"), do: 0.66
+  def normalize_amount("1/2"), do: 0.5
+  def normalize_amount("1/4"), do: 0.25
+  def normalize_amount("3/4"), do: 0.75
+  def normalize_amount("1/3"), do: 0.33
+  def normalize_amount("2/3"), do: 0.66
 
-  defp float_or_zero(string) do
+  def normalize_amount(string) do
     case Float.parse(string) do
       {res, _} -> res
       :error -> 0.0
+    end
+  end
+
+  def normalize_measurement(msmt) do
+    case msmt do
+      "cup" -> "cup"
+      "cups" -> "cup"
+      "c" -> "cup"
+      "gallon" -> "gallon"
+      "gallons" -> "gallon"
+      "g" -> "gram"
+      "gram" -> "gram"
+      "liter" -> "liter"
+      "litre" -> "liter"
+      "ounce" -> "oz"
+      "ounces" -> "oz"
+      "oz" -> "oz"
+      "pound" -> "pound"
+      "pounds" -> "pound"
+      "lb" -> "pound"
+      "lbs" -> "pound"
+      "tablespoon" -> "tbsp"
+      "tablespoons" -> "tbsp"
+      "tbsp" -> "tbsp"
+      "teaspoon" -> "tsp"
+      "teaspoons" -> "tsp"
+      "tsp" -> "tsp"
+      _ -> "unit"
     end
   end
 end
--- lib/optimalist/scraper/wordpress.ex
+++ lib/optimalist/scraper/wordpress.ex
@@ -1,4 +1,6 @@
 defmodule Optimalist.Scraper.WordPress do
+  alias Optimalist.Scraper
+
   def parse(html) do
     with nodes <- Floki.find(html, ".wprm-recipe-ingredient") do
       nodes
@@ -8,7 +10,11 @@ defmodule Optimalist.Scraper.WordPress do
           |> Enum.map(&Floki.find(node, &1))
           |> Enum.map(&Floki.text/1)
 
-        %{name: name, measurement: measurement, amount: amount}
+        %{
+          name: name,
+          measurement: Scraper.normalize_measurement(measurement),
+          amount: Scraper.normalize_amount(amount)
+        }
       end)
     end
   end
